5c1b5d11e3817929c054364f9d04328799219369
Tilda Källström <tildakallstrom@gmail.com>
2021-11-15 09:43:53 +0100

    first commit

---
+++ ToDoListApp/TodoItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoListApp
+{
+    //class 
+    public class TodoItem
+    {
+        //string with getter and setter
+        public string TodoText { get; set; }
+        //bool with getter and setter
+        public bool Complete { get; set; }
+
+        public TodoItem(string TodoText, bool Complete)
+        {
+            //todoitem contains todotext and completed or not
+            this.TodoText = TodoText;
+            this.Complete = Complete;
+           
+        }
+    }
+}
---
+++ ToDoListApp/TodoListViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+//lets me use ICommand
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ToDoListApp
+{
+    //class viewmodel
+    class TodoListViewModel
+    {
+        public ObservableCollection<TodoItem> TodoItems { get; set; }
+        public TodoListViewModel()
+        {
+            TodoItems = new ObservableCollection<TodoItem>();
+
+        }
+        //command to add todo
+        public ICommand AddTodoCommand => new Command(AddTodoItem);
+        //string newtodoinputvalue with getter and setter
+        public string NewTodoInputValue { get; set; }
+        //add todo
+        void AddTodoItem()
+        {
+            TodoItems.Add(new TodoItem(NewTodoInputValue, false));
+        }
+        //command to remove todo
+        public ICommand RemoveTodoCommand => new Command(RemoveTodoItem);
+        public string RemoveTodoInputValue { get; set; }
+        //method to remove chosen todo
+        void RemoveTodoItem(object o)
+        {
+            TodoItem todoItemBeingRemoved = o as TodoItem;
+            TodoItems.Remove(todoItemBeingRemoved);
+        }
+
+    }
+}
